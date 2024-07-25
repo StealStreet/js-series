e10f0032a3583b8d2933c89945e9a02e0181d99a
Sk Sofiquee Fiaz <142611365+StealStreet@users.noreply.github.com>
2024-07-25 01:44:38 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // 
+
+let name = "sofiquee"
+let age = 22
+let state;
+
+// TASK :: checkout the specification of these datatype in 
+// ECMAScript website
+
+// primitive datatype
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// non-primitive datatype
+// object
+// string => ""
+
+console.log(typeof state)
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
